8a8e0f559241d5cba698af4fe592a71574a67ec1
sweewo <vlad.no.sokol@gmail.com>
2020-05-06 23:12:06 +0300

    kubernetes deployment update

--- kubernetes/deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-starts=$1
-file=$2
-env | grep $starts > _temp
-sed -i 's#="#: "#g' _temp
-sed -i 's/^/  /g' _temp
-sed -i "s/$starts//g" _temp
-sed -i '/GAMESTORE_ENV_DATA/r _temp' $file
-sed -i '/GAMESTORE_ENV_DATA/d' $file
